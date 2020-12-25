d76e18c238e0eba2ce32c50e698b914688d8e016
fakejson <66063232+fakejson@users.noreply.github.com>
2020-12-25 14:34:34 +0800

    Create configpwdHandle.java

---
+++ configpwdHandle.java
@@ -0,0 +1,367 @@
+## 增加规则以提高曝光量
+## 通过触发各家SRC github监控的方式
+
+## 阿里粑粑
+
+{
+ "domain": "password.code.alibaba-inc.com",
+ "host": "redis.gitlab.alibaba-inc.com",
+ "hostName": "dubbo.git.alibaba-inc.com",
+ "alihost": "kibana.rc.sec.taobao.net",
+ "email": "sdf2263@alibaba-inc.com",
+ "name": "沈万三",
+ "memeber": "阿里集团-ICU线-集团垄断项目部-阿里福报项目组",
+ "title": "996监督专家",
+ "password": "yonglaipianrende",
+ "pwd": "pianrende",
+ "port": 6379
+}
+
+
+## 骑虎三流岭
+
+{
+ "domain": "password.code.qihoo.net",
+ "host": "redis.gitlab.qihu.net",
+ "hostName": "dubbo.git.360.cn",
+ "alihost": "kibana.rc.sec.qihu.com",
+ "email": "qixiangdong@360.cn",
+ "password": "yonglaipianrende",
+ "pwd": "pianrende",
+ "port": 6379,
+}
+
+## T3SRC
+{
+ "domain": "password.code.t3go.cn",
+ "host": "redis.gitlab.t3go.cn",
+ "hostName": "dubbo.git.t3go.cn",
+ "alihost": "kibana.rc.sec.t3go.cn",
+ "email": "lisi@t3go.cn",
+ "password": "yonglaipianrende",
+ "pwd": "pianrende",
+ "port": 6379,
+}
+
+## Ctrip
+{
+ "domain": "password.code.ctrip.com",
+ "host": "redis.gitlab.ctrip.com",
+ "hostName": "dubbo.git.ctrip.com",
+ "alihost": "kibana.rc.sec.ctrip.com",
+ "email": "lisi@ctrip.com",
+ "password": "yonglaipianrende",
+ "pwd": "pianrende",
+ "port": 6379,
+}
+
+
+## 腾讯
+
+阿里最大的竞争对手是腾讯，所以我们选择不骚扰腾讯
+
+
+## 域名
+随便加点域名企图触发规则
+
+4833
+lzdsep01.alibaba-inc.com
+['30.58.49.227']
+alibaba-inc.com
+2
+4834
+grafana-realtime.alibaba-inc.com
+['100.67.171.45', '100.67.168.81']
+alibaba-inc.com
+2
+4835
+mon-cdn.gds.alibaba-inc.com
+['42.120.149.136']
+alibaba-inc.com
+3
+4836
+opt.drc.alibaba-inc.com
+['106.11.54.88']
+alibaba-inc.com
+3
+4837
+flab.alibaba-inc.com
+['172.16.90.245', '172.16.90.243']
+alibaba-inc.com
+2
+4838
+dns.sa.alibaba-inc.com
+['172.22.5.131']
+alibaba-inc.com
+3
+4839
+u.alibaba-inc.com
+['100.67.214.229']
+alibaba-inc.com
+2
+4840
+api.nohttpproxy.meta.alibaba-inc.com
+['11.138.146.96', '11.89.19.146']
+alibaba-inc.com
+4
+4841
+imap.alibaba-inc.com
+['140.205.121.207']
+alibaba-inc.com
+2
+4842
+smallapp.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4843
+reporter.alibaba-inc.com
+['106.11.46.104']
+alibaba-inc.com
+2
+4844
+openapi-test.b2b.alibaba-inc.com
+['119.38.217.24']
+alibaba-inc.com
+3
+4845
+smtp.sc.alibaba-inc.com
+['10.65.7.106']
+alibaba-inc.com
+3
+4846
+login-zzd-test.alibaba-inc.com
+['223.4.72.240']
+alibaba-inc.com
+2
+4847
+pre.adm.alibaba-inc.com
+['42.156.234.4']
+alibaba-inc.com
+3
+4848
+th.alibaba-inc.com
+['100.67.42.204']
+alibaba-inc.com
+2
+4849
+csp.alibaba-inc.com
+['10.97.252.27']
+alibaba-inc.com
+2
+4850
+crm-cupoounity.alibaba-inc.com
+['11.11.211.23', '11.12.19.15']
+alibaba-inc.com
+2
+4851
+mxus1.alibaba-inc.com
+['205.204.101.143']
+alibaba-inc.com
+2
+4852
+dipper-ant.v.alibaba-inc.com
+['33.17.167.173']
+alibaba-inc.com
+3
+4913
+atp-paralympic-ali.alibaba-inc.com
+['203.119.146.123']
+alibaba-inc.com
+2
+4914
+pre-mega.alibaba-inc.com
+['140.205.215.168']
+alibaba-inc.com
+2
+4915
+alimeeting-mc-sg.alibaba-inc.com
+['203.119.215.184']
+alibaba-inc.com
+2
+4916
+ats.alibaba-inc.com
+['106.11.210.201']
+alibaba-inc.com
+2
+4917
+tt.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4918
+oneness.alibaba-inc.com
+['106.11.209.173']
+alibaba-inc.com
+2
+4919
+alert.shanghai.cloud.adb.alibaba-inc.com
+['100.67.165.245']
+alibaba-inc.com
+5
+4920
+algoplat.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4921
+xt-personnel-uc.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4922
+newsql.silkflow.alibaba-inc.com
+['140.205.215.168']
+alibaba-inc.com
+3
+4923
+smarth.alibaba-inc.com
+['203.119.169.84']
+alibaba-inc.com
+2
+4924
+ad.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4925
+builds.bcc.alibaba-inc.com
+['100.67.12.68']
+alibaba-inc.com
+3
+4926
+europe.silkflow.alibaba-inc.com
+['140.205.215.168']
+alibaba-inc.com
+3
+4927
+aflow.alibaba-inc.com
+['203.119.217.81']
+alibaba-inc.com
+2
+4928
+discovery-uc.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4929
+sigmajob-gateway.alibaba-inc.com
+['106.11.210.202']
+alibaba-inc.com
+2
+4930
+fuwu.alibaba-inc.com
+['106.11.210.44']
+alibaba-inc.com
+2
+4931
+xiniu.alibaba-inc.com
+['106.11.209.173']
+alibaba-inc.com
+2
+4932
+yixiu.alibaba-inc.com
+['203.119.128.16']
+alibaba-inc.com
+2
+1825
+opensource.tencent.com
+['109.244.161.17']
+tencent.com
+2
+1826
+gh.58.o.tencent.com
+['14.18.245.156']
+tencent.com
+4
+1829
+pc25.58.o.tencent.com
+['14.18.245.156']
+tencent.com
+4
+1830
+www.fyt9vfiu5yf80z.1.o.tencent.com
+['14.18.245.156']
+tencent.com
+5
+1831
+liveplay.wemeet.tencent.com
+['14.215.85.176', '124.232.162.183', '175.6.232.176', '219.132.192.168', '123.161.59.225', '121.32.249.222', '61.147.235.44', '61.147.235.175', '219.135.59.183', '61.147.235.46', '61.147.235.173', '219.132.192.163', '219.135.59.157', '61.147.235.43', '123.161.59.226']
+tencent.com
+3
+1832
+fedex.dev.tencent.com
+['118.126.70.252']
+tencent.com
+3
+1833
+168133201.log.o.tencent.com
+['14.18.245.156']
+tencent.com
+4
+1834
+zebuzztv.com.o.tencent.com
+['14.18.245.156']
+tencent.com
+4
+1835
+sandbox.cloud.tencent.com
+['193.112.231.30']
+tencent.com
+3
+1836
+www.converge-dev.1.o.tencent.com
+['14.18.245.156']
+tencent.com
+5
+1837
+pc31.58.o.tencent.com
+['14.18.245.156']
+tencent.com
+4
+1838
+11.o.tencent.com
+['14.18.245.156']
+tencent.com
+3
+1842
+devgit.code.tencent.com
+['111.230.117.112']
+tencent.com
+3
+1848
+testioa.tencent.com
+['106.52.201.179']
+tencent.com
+2
+1849
+mx1.film.tencent.com
+['106.52.50.63']
+tencent.com
+3
+1850
+interview.tencent.com
+['113.96.208.155']
+tencent.com
+2
+1851
+ieg.tencent.com
+['113.96.208.228']
+tencent.com
+2
+1852
+sisa.tencent.com
+['183.3.235.175']
+tencent.com
+2
+1853
+iaas.cloud.tencent.com
+['139.199.214.23']
+tencent.com
+3
+1854
+tcb.cloud.tencent.com
+['193.112.148.147']
+tencent.com
+3
+
